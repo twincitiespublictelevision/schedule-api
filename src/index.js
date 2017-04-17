5251ce9cc1bf553359048f51d40475686f72668e
Travis Ingram <ingramtravis@gmail.com>
2017-04-17 17:15:56 -0500

    scheduleAPIv2 index.js
    Add server initialization with port normalization,
    basic error handeling

---
+++ src/index.js
@@ -0,0 +1,45 @@
+'use strict'
+
+import * as http from 'http';
+import api from './api';
+
+const app = new api();
+const DEFAULT_PORT = 3000;
+const port = normalizePort(process.env.PORT);
+const server = http.createServer(app.express);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+	let port = (typeof val === 'string') ? parseInt(val, 10) : val;
+
+	if (port && isNaN(port)) return port;
+	else if (port >= 0) return port;
+	else return DEFAULT_PORT;
+}
+
+function onError(error) {
+	if (error.syscall !== 'listen') throw error;
+	let bind = (typeof port === 'string') ? `Pipe ${port}` : `Port ${port.toString()}`;
+
+	switch (error.code) {
+		case 'EACCESS':
+			console.error(`${bind} is already in use.`);
+			process.exit(1);
+			break
+		case 'EADDRINUSE':
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;	
+		default:
+			throw error;
+	}
+}
+
+function onListening() {
+	let addr = server.address();
+	let bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
+	console.log(`Listening on ${bind}`);
+}
