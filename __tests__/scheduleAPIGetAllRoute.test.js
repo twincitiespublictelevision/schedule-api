1618e4b698339447748526abbf946733bcaf9d0f
Travis Ingram <ingramtravis@gmail.com>
2017-05-22 14:36:15 -0500

    scheduleAPIv2 scheduleAPIGetAllRoute.test.js
    Add initial tests for get all route

---
+++ __tests__/scheduleAPIGetAllRoute.test.js
@@ -0,0 +1,45 @@
+import request from 'supertest';
+import api from '../src/api.js';
+
+const app = new api().express;
+
+describe('scheduleAPI', function() {
+
+	describe('GET /api/v1/channels - get all airings', function() {
+		// Tests the expected properties on the response object.
+		let expectedProps = ['airing'];
+
+		it('should return a JSON array', function() {
+			return request(app).get('/api/v1/channels')
+			.expect(200)
+			.then(function(res) {
+				// Check that an array is returned
+				expect(res.body).toBeInstanceOf(Array);
+			});
+		});
+
+		it('should return objects with the correct properties', function() {
+			return request(app).get('/api/v1/channels')
+			.expect(200)
+			.then(function(res) {
+				// Check for the expected properties
+				let sampleKeys = Object.keys(res.body[0]);
+				expectedProps.forEach(function(key) {
+					expect(sampleKeys.includes(key)).toBe(true);
+				});
+			});
+		});
+
+		it('shouldn\'t return objects with extra properties', function() {
+			return request(app).get('/api/v1/channels')
+			.expect(200)
+			.then(function(res) {
+				// Check only for the expected properties
+				let extraProps = Object.keys(res.body[0]).filter(function(key) {
+					return !expectedProps.includes(key);
+				});
+				expect(extraProps.length).toBe(0);
+			});
+		});
+	});
+});
