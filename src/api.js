f84eb1685f7ae2d3d0319e3ac09b2b8767c15469
Travis Ingram <ingramtravis@gmail.com>
2017-05-22 14:19:49 -0500

    scheduleAPIv2 api.js
    Add Node fs module to write logged server events on base route
    to file,
    Add verbose, Apache like logs in JSON format, to base route.

--- src/api.js
+++ src/api.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
-
+import * as fs from 'fs';
 import ChannelRouter from './routes/channelRouter';
+
+const serverLogsPath = __dirname + '/../data/logFiles/serverLogs/serverAccess.log';
+const accessLogStream = fs.createWriteStream(serverLogsPath, {flags: 'a'});
 
 export default class api {
 
@@ -14,9 +17,22 @@ export default class api {
 
 	//register middleware
 	middleware() {
-		this.express.use(morgan('dev'));
 		this.express.use(bodyParser.json());
 		this.express.use(bodyParser.urlencoded({ extended: false }));
+		this.express.use(morgan('dev'));
+		this.express.use(morgan('{' +
+		'"remote_addr": ":remote-addr",' +
+		'"remote_user": ":remote-user",' +
+		'"date": ":date[clf]",' +
+		'"method": ":method",' +
+		'"url": ":url",' +
+		'"http_version": ":http-version",' +
+		'"status": ":status",' +
+		'"result_length": ":res[content-length]",' +
+		'"referrer": ":referrer",' +
+		'"user_agent": ":user-agent",' +
+		'"response_time": ":response-time" }',
+		{stream: accessLogStream}));
 	}
 
 	//connect resource routers
