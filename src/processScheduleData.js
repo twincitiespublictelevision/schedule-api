65d0abb4a333870aee934e25d319551b621c3698
Travis Ingram <ingramtravis@gmail.com>
2017-06-07 17:47:41 -0500

    scheduleAPIv2 processScheduleData.js
    Add path module to enable parsing, appending to files,
    Update function to get, save, delete a set of file paths

--- src/processScheduleData.js
+++ src/processScheduleData.js
@@ -1,6 +1,7 @@
 
 import mongoose from 'mongoose';
 import * as fs from 'fs';
+import * as path from 'path';
 import { jsonScheduleData } from './helpers/convertXMLToJSON';
 
 import {
@@ -21,34 +22,37 @@ import {
 	getCurrentDate,
 	saveParsedFile,
 	moveScheduleDataFileString,
+	moveRawScheduleDataFile,
 	moveScheduleDataFileArray,
 	removeFile,
 	removeSingleFile,
 	verifyItemIsAFile,
 	getFileType,
-	// getAllDirectoryFiles,
+	filterFilePaths,
 	getFilePaths,
 	extractScheduleData
 } from './helpers/importScheduleData';
 
-// const proTrackScheduleData 				= (__dirname + '/../../data/tpt_full_schedule_example.xml');
-// const sampleProTrackScheduleData 	= (__dirname + '/../../data/sampleSchedule.xml');
-// const smallSamplePTScheduleData 	= (__dirname + '/../../data/smallSampleSchedule.xml');
+createDirectoryPath(process.env.WATCH_DIR);
 
-// getFilePaths(process.env.WATCH_DIR, '.xml', function(error) {
-// 	if (error) {
-// 		console.log(error);
-// 	}
-// 		console.log('Maybe??');
-// });
+beginWatchingDirectory(process.env.WATCH_DIR);
 
-// createDirectoryPath(process.env.WATCH_DIR);
-
-// beginWatchingDirectory(process.env.WATCH_DIR);
-
-// verifyItemIsAFile(process.env.ACCESS_LOG);
-
-getFileType(process.env.ACCESS_LOG);
+getFilePaths(process.env.WATCH_DIR, '.xml', fs.readdir, function(error, joinedFilePaths) {
+	if (error) {
+		console.log(error)
+	} else {
+		moveRawScheduleDataFile(joinedFilePaths, [process.env.BACKUP_DIR, process.env.WORKING_DIR], function(error, sourceFilePath) {
+			if (error) {
+				console.log(error)
+			}
+		});
+		removeFile(joinedFilePaths, function(error) {
+			if (error) {
+				console.log(error);
+			}
+		});
+	}
+});
 
 /*
 
@@ -62,9 +66,13 @@ watch origination directory for file changes
 
 	If changes, announce type of chagne to parent process
 
-Read directory contents, create list from file names
+Read directory contents, adding file names to a list
 
-	Report file names to partent process
+	Report each file name to partent process
+
+Once the file threshold is reached, pass on the list
+
+Write / save list of files to working / backup directories
 
 Pass file names to parser for converstion from XML to JSON
 
@@ -77,6 +85,5 @@ For each parsed file, extract schedule object
 Save schedule object to db
 
 	Report each object saved to parent process
-
 
 */
