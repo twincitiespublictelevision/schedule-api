56c108d416a319770ce8f5e5fb7ffc6484ee8f5d
Travis Ingram <ingramtravis@gmail.com>
2017-06-07 17:43:30 -0500

    scheduleAPIv2 startProcess.js
    Update formatting of console message template literals with line breaks, PID

--- startProcess.js
+++ startProcess.js
@@ -10,23 +10,23 @@ let spawn = require('child_process').spawn;
 const launchApi = spawn('node', ['build/index.js']);
 
 launchApi.stdin.on('data', (data) => {
-	console.log(`stdin of ${launchApi.pid}: ${data}`);
+	console.log(`stdin of Schedule API, PID ${launchApi.pid}: \n${data}`);
 });
 
 launchApi.stdout.on('data', (data) => {
-	console.log(`stdout from process ${launchApi.pid}: ${data}`);
+	console.log(`stdout of Schedule API, PID ${launchApi.pid}: \n${data}`);
 });
 
 launchApi.stderr.on('data', (data) => {
 	if (data !== null) {
-		console.log(`spawn error from process ${launchApi.pid}: ${data.toString()}`);
+		console.log(`spawn error from Schedule API PID ${launchApi.pid}: \n${data.toString()}`);
 	} else {
 		console.log(`stderr: ${data}`);
 	}
 });
 
 launchApi.on('close', (code) => {
-	console.log(`child process exited with a code of ${code}`);
+	console.log(`Schedule API PID ${launchApi.pid} exited with a code of ${code}`);
 });
 
 // Launch File import, listen on StandardOut for messages.
@@ -34,21 +34,21 @@ launchApi.on('close', (code) => {
 const fileImport = spawn('node', ['build/processScheduleData.js']);
 
 fileImport.stdin.on('data', (data) => {
-	console.log(`stdin of ${fileImport.pid}: ${data}`);
+	console.log(`stdin of File I/O, PID ${fileImport.pid}: \n${data}`);
 });
 
 fileImport.stdout.on('data', (data) => {
-	console.log(`stdout from process ${fileImport.pid}: ${data}`);
+	console.log(`stdout of File I/O, PID ${fileImport.pid}: \n${data}`);
 });
 
 fileImport.stderr.on('data', (data) => {
 	if (data !== null) {
-		console.log(`spawn error from process ${fileImport.pid}: ${data.toString()}`);
+		console.log(`Spawn error from File I/O PID ${fileImport.pid}: \n${data.toString()}`);
 	} else {
 		console.log(`stderr: ${data}`);
 	}
 });
 
 fileImport.on('close', (code) => {
-	console.log(`child process exited with a code of ${code}`);
+	console.log(`File I/O PID ${fileImport.pid} exited with a code of ${code}`);
 });
