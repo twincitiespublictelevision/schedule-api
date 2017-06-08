a3822da4947111f23456a0c1430e6eb1a02dbbaa
Travis Ingram <ingramtravis@gmail.com>
2017-06-08 17:45:54 -0500

    scheduleAPIv2 startProcess.js
    Create additional watch processes, conditions for initiating them

--- startProcess.js
+++ startProcess.js
@@ -7,7 +7,9 @@ let spawn = require('child_process').spawn;
 
 // Launch API, listen on StandardOut for messages.
 
-const launchApi = spawn('node', ['build/index.js']);
+const launchApi = spawn('node', ['build/index.js'], {
+	stdio: ['pipe', 'pipe', 'pipe', 'ipc']
+});
 
 launchApi.stdin.on('data', (data) => {
 	console.log(`stdin of Schedule API, PID ${launchApi.pid}: \n${data}`);
@@ -29,9 +31,11 @@ launchApi.on('close', (code) => {
 	console.log(`Schedule API PID ${launchApi.pid} exited with a code of ${code}`);
 });
 
-// Launch File import, listen on StandardOut for messages.
+// Launch directory monitor on WATCH_DIR, listen on Standard In for messages.
 
-const fileImport = spawn('node', ['build/processScheduleData.js']);
+const fileImport = spawn('node', ['build/processScheduleData.js'], {
+	stdio: ['pipe', 'pipe', 'pipe', 'ipc']
+});
 
 fileImport.stdin.on('data', (data) => {
 	console.log(`stdin of File I/O, PID ${fileImport.pid}: \n${data}`);
@@ -49,6 +53,80 @@ fileImport.stderr.on('data', (data) => {
 	}
 });
 
+fileImport.on('message', (message) => {
+	if(message.start === true) {
+		fileMoveSaveDelete();
+	} else {
+		console.log('Received unexpected message.');
+	}
+});
+
 fileImport.on('close', (code) => {
 	console.log(`File I/O PID ${fileImport.pid} exited with a code of ${code}`);
 });
+
+// Launch file move, save, delete process
+
+function fileMoveSaveDelete() {
+	const moveSaveDeleteFile = spawn('node', ['build/moveSaveDeleteFile.js'], {
+		stdio: ['pipe', 'pipe', 'pipe', 'ipc']
+	});
+
+	moveSaveDeleteFile.stdout.on('data', (data) => {
+		console.log(`stdout of moveSaveDeleteFile, PID ${moveSaveDeleteFile.pid}: \n${data}`);
+	});
+
+	moveSaveDeleteFile.stdout.on('message', (message) => {
+		moveSaveDeleteFile.disconnect();
+	});
+}
+
+// Launch directory monitor on WORKING_DIR, listen on Standard In for messages.
+
+const fileParseExtractSendToDb = spawn('node', ['build/watchWorkingDir.js'], {
+	stdio: ['pipe', 'pipe', 'pipe', 'ipc']
+});
+
+fileParseExtractSendToDb.stdin.on('data', (data) => {
+	console.log(`stdin of Parse File, PID ${fileParseExtractSendToDb.pid}: \n${data}`);
+});
+
+fileParseExtractSendToDb.stdout.on('data', (data) => {
+	console.log(`stdout of Parse File, PID ${fileParseExtractSendToDb.pid}: \n${data}`);
+});
+
+fileParseExtractSendToDb.stderr.on('data', (data) => {
+	if (data !== null) {
+		console.log(`Spawn error from Parse File PID ${fileParseExtractSendToDb.pid}: \n${data.toString()}`);
+	} else {
+		console.log(`stderr: ${data}`);
+	}
+});
+
+fileParseExtractSendToDb.on('message', (message) => {
+	if(message.start === true) {
+		fileParseExtractSend();
+	} else {
+		console.log('Received unexpected message.');
+	}
+});
+
+fileParseExtractSendToDb.on('close', (code) => {
+	console.log(`Parse File PID ${fileParseExtractSendToDb.pid} exited with a code of ${code}`);
+});
+
+// Parse XML file, extracting object and saving it to the DB
+
+function fileParseExtractSend() {
+	const parseExtractSend = spawn('node', ['build/parseFileSaveData.js'], {
+		stdio: ['pipe', 'pipe', 'pipe', 'ipc']
+	});
+
+	parseExtractSend.stdout.on('data', (data) => {
+		console.log(`stdout of parseExtractSend, PID ${parseExtractSend.pid}: \n${data}`);
+	});
+
+	parseExtractSend.stdout.on('message', (message) => {
+		parseExtractSend.disconnect();
+	});
+}
