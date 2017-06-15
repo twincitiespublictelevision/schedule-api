7f6fdd587010fab6801b146f960b63442c5cbb41
Travis Ingram <ingramtravis@gmail.com>
2017-06-15 17:40:21 -0500

    scheduleAPIv2 moveSaveDelete.js
    Comment out all functions to isloate issues,
    may move contents to importScheduleData for consistency

--- src/moveSaveDeleteFile.js
+++ src/moveSaveDeleteFile.js
@@ -3,9 +3,6 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 import {
-	baseDirectory,
-	backupDataDirectoryPath,
-	currentDataDirectoryPath,
 	createDirectoryPath,
 	verifyFilePath,
 	beginWatchingDirectory,
@@ -24,21 +21,86 @@ import {
 	extractScheduleData
 } from './helpers/importScheduleData';
 
-getFilePaths(process.env.WATCH_DIR, '.xml', fs.readdir, function(error, joinedFilePaths) {
-	if (error) {
-		console.log(error)
-	} else {
-		moveRawScheduleDataFile(joinedFilePaths, [process.env.BACKUP_DIR, process.env.WORKING_DIR], function(error, sourceFilePath) {
-			if (error) {
-				console.log(error)
-			}
-		});
-		removeFile(joinedFilePaths, function(error) {
-			if (error) {
-				console.log(error);
-			}
-		});
-	}
-	console.log('Task finished, closing process.');
-	process.send({finished: true});
-});
+// module.exports = function moveFile(file) {
+// 	let currentDate = getCurrentDate();
+
+// 	fs.readFile(file, function(error, data) {
+// 		if (error) {
+// 			console.log(error);
+// 		}
+// 		fs.writeFile(process.env.BACKUP_DIR + currentDate + path.basename(file), data, function(error) {
+// 			if (error) {
+// 				console.log(error);
+// 			}
+// 			fs.writeFile(process.env.WORKING_DIR + currentDate + path.basename(file), data, function(error) {
+// 				if (error) {
+// 					console.log(error);
+// 				}
+// 				fs.unlink(file, function(error) {
+// 					if (error) {
+// 						console.log(error);	
+// 					}
+// 					console.log(`Deleted ${path.basename(file)}.`);
+// 				});
+// 			});
+// 		});
+// 	});
+
+	// moveRawScheduleDataFile(file, [process.env.BACKUP_DIR, process.env.WORKING_DIR], function(error, sourceFilePath) {
+	// 			if (error) {
+	// 				console.log(error);
+	// 			} else {
+	// 				removeFile(sourceFilePath, function(error) {
+	// 					if (error) {
+	// 						console.log(error);
+	// 					} else {
+	// 						console.log(`${sourceFilePath}.`);
+	// 					}
+	// 				});
+	// 				// console.log(`${sourceFilePath}.`);
+	// 				// console.log(`${progress}MSD task finished, closing process.`);
+	// 			}
+				
+	// 			// console.log(`${progress}MSD task finished, closing process.`);
+	// 			// process.send({finished: true});
+	// 			// process.send({'finished': true}, 'ipc');
+	// 			// process.send('Hello From MSD');
+	// 			// process.disconnect();
+
+	// 		});	
+// }
+
+// getFilePaths(process.env.WATCH_DIR, '.xml', fs.readdir, function(error, joinedFilePaths) {
+// 	if (error) {
+// 		console.log(error);
+// 	} else {
+
+// 		joinedFilePaths.forEach(function(file, index) {
+// 			console.log(`Moving ${file}.`);
+
+// 			moveRawScheduleDataFile(file, [process.env.BACKUP_DIR, process.env.WORKING_DIR], function(error, sourceFilePath) {
+// 				if (error) {
+// 					console.log(error);
+// 				} else {
+// 					removeFile(sourceFilePath, function(error) {
+// 						if (error) {
+// 							console.log(error);
+// 						} else {
+// 							console.log(`${sourceFilePath}.`);
+// 						}
+// 					});
+// 					// console.log(`${sourceFilePath}.`);
+// 					// console.log(`${progress}MSD task finished, closing process.`);
+// 				}
+				
+// 				// console.log(`${progress}MSD task finished, closing process.`);
+// 				// process.send({finished: true});
+// 				// process.send({'finished': true}, 'ipc');
+// 				// process.send('Hello From MSD');
+// 				// process.disconnect();
+
+// 			});
+// 			// removeSingleFile(file);
+// 		});
+// 	}
+// });
