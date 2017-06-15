e76d30c8c04803a82e887ef19f95c62e5f8a927c
Travis Ingram <ingramtravis@gmail.com>
2017-06-15 17:31:46 -0500

    scheduleAPIv2 gulpfile.js
    Add new task to process / watch startProcess in base dir.

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,21 @@ gulp.task('scripts', () => {
 	.pipe(gulp.dest('build'));
 });
 
+gulp.task('baseDir', () => {
+	return gulp.src('./startProcess.js')
+	.pipe(sourcemaps.init())
+	.pipe(babel())
+	.pipe(sourcemaps.write('.'))
+	.pipe(gulp.dest('build'));
+});
+
 gulp.task('watch', ['scripts'], () => {
 	gulp.watch('src/**/*.js', ['scripts']);
 });
 
+gulp.task('watchBase', ['baseDir'], () => {
+	gulp.watch('./startProcess.js', ['baseDir']);
+});
+
 gulp.task('default', ['watch']);
+gulp.task('build', ['watch', 'watchBase']);
