21b1c1e85f850e2c152c925029e5a91bcb28fff1
Travis Ingram <ingramtravis@gmail.com>
2017-06-15 17:38:48 -0500

    scheduleAPIv2 parseFileSaveData.js
    Add new function that parses and saves a single file,
    previous implementation was too brittle

--- src/parseFileSaveData.js
+++ src/parseFileSaveData.js
@@ -7,13 +7,12 @@ import { jsonScheduleData } from './helpers/convertXMLToJSON';
 import {
 	mongoURI,
 	mongoDB,
-	removeAllInsert
+	multipleInsert,
+	removeAllInsert,
+	removeAll,
 	} from './database';
 
 import {
-	baseDirectory,
-	backupDataDirectoryPath,
-	currentDataDirectoryPath,
 	createDirectoryPath,
 	verifyFilePath,
 	beginWatchingDirectory,
@@ -32,63 +31,70 @@ import {
 	extractScheduleData
 } from './helpers/importScheduleData';
 
-function removeScheduleData(mongoDB, data, callback) {
-	let scheduleCollection = mongoDB.collection('scheudleData');
-
-	scheduleCollection.remove({}, function(error) {
-		if (error) {
-			console.log(error);
-		}
-	});
-}
-
-function insertAllScheduleData(mongoDB, fullScheduleData, callback) {
+function timedMultipleInsert(mongoDB, fullScheduleData, file, callback) {
 	let scheduleCollection = mongoDB.collection('scheduleData');
-
-	scheduleCollection.insertMany(fullScheduleData, function(error, result) {
-		if(error) {
-			console.log(error);
-		}
-		return result;
-	});
-};
-
-function timedInsertScheduleData(mongoDB, fullScheduleData, callback) {
-	let scheduleCollection = mongoDB.collection('scheduleData');
-	let start = Date.now();
+	let removeStart = Date.now();
 
 	scheduleCollection.remove({}, function(){
-		console.log('Removed documents. ', Date.now() - start);
-		start = Date.now();
+		console.log(`Removing ${file} documents took: ${Date.now() - removeStart} ms`);
+		let addingStart = Date.now();
 		scheduleCollection.insertMany(fullScheduleData, function(error, result) {
 			if(error) {
 				console.log(error);
 			}
-				console.log('Inserted documents. ', Date.now() - start);
+				console.log(`Adding ${file} documents took: ${Date.now() - addingStart} ms`);
 			});
 	});
 };
 
-getFilePaths(process.env.WORKING_DIR, '.xml', fs.readdir, function(error, joinedFilePaths) {
-	if (error) {
-		console.log(error)
-	} else {
-		joinedFilePaths.forEach(function(file) {
-			jsonScheduleData(file, function(error, parseData) {
-				if (error) {
-					console.log(error);
-				} else {
-					let scheduleData = extractScheduleData(parseData);
-					insertAllScheduleData(mongoDB, scheduleData);
-				}
-			});
-			removeFile(joinedFilePaths, function(error) {
-				if (error) {
-					console.log(error);
-				}
-			});
-		});
-	}
-	console.log('Task finished, closing process.');
-	process.send({finished: true});
-});
+module.exports = function handleFile(file) {
+	let filename = path.basename(file);
+
+	jsonScheduleData(file, function(error, parseData) {
+		if (error) {
+			console.log(error);
+		} else {
+			let scheduleData = extractScheduleData(parseData);
+			console.log(`Starting DB write of ${filename}.`);
+			timedMultipleInsert(mongoDB, scheduleData, filename);
+		}
+		removeSingleFile(file);
+	});
+}
+
+// getFilePaths(process.env.WORKING_DIR, '.xml', fs.readdir, function(error, joinedFilePaths) {
+// 	if (error) {
+// 		console.log(error)
+// 	} else {
+// 		let progress = joinedFilePaths.map(function() {
+// 			return 1;
+// 		});
+
+// 			joinedFilePaths.forEach(function(file, index) {
+// 				console.log(`Begin parsing ${file}.`);
+
+// 				jsonScheduleData(file, function(error, parseData) {
+// 					if (error) {
+// 						console.log(error);
+// 					} else {
+// 						let scheduleData = extractScheduleData(parseData);
+// 						timedMultipleInsert(mongoDB, scheduleData);
+// 					}
+// 					removeSingleFile(file);
+
+// 					progress[index] = 0;
+
+// 					if (progress.reduce(function(sum, current) {
+// 						return sum + current;
+// 						}, 0) === 0) {
+
+// 						console.log('PSE task finished, closing process.');
+// 						process.send({finished: true});
+// 						// process.exitCode(1);
+// 					}
+
+// 						// console.log(`Parse progress value ending, ${progress}`);
+// 				});
+// 			});
+// 		}
+// });
