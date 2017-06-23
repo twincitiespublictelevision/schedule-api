539e71a84cc3eaa8a78f7ee0da3822496997eca0
Travis Ingram <ingramtravis@gmail.com>
2017-06-23 17:52:46 -0500

    scheduleAPIv2 channelRouter.js
    Replace channel with series, add initial endpoints  for series

--- src/routes/channelRouter.js
+++ src/routes/channelRouter.js
@@ -1,3 +1,4 @@
+
 import mongodb from 'mongodb';
 import {
 	mongoDB,
@@ -6,10 +7,10 @@ import {
 
 import { Router } from 'express';
 
-export default class ChannelRouter {
+export default class SeriesRouter {
 
 	// We use the mount path as the constructor argument
-	constructor(path = '/api/v1/channels') {
+	constructor(path = '/api/v1/series') {
 		// Instantiate the express.Router
 		this.router = Router();
 		this.path = path;
@@ -17,16 +18,104 @@ export default class ChannelRouter {
 	}
 
 	/**
-	 * Return all data in channels
+	 * Return a series by its ID number
+	 * @description example URL - http://localhost:3000/api/v1/series/45
+	 * @ param {String} request - the request string
+	 * @ param {String} response - the response string
+	 * @ member {Function} scheduleCollection - links to mongoDB collection
+	 * @ external {}
 	 */
-	getAll(request, response) {
+	getSeriesById(request, response) {
 		let scheduleCollection = mongoDB.collection('scheduleData');
+		let seriesID = parseInt(request.params.id);
 
-		scheduleCollection.find().limit( 20 ).toArray(function(error, docs) {
+		scheduleCollection.find( {
+			'series.series_id' : seriesID
+		} )
+		.limit( 10 )
+		.toArray(function(error, docs) {
 			if (error) {
 				console.log(error);
 			}
-			response.status(200).json(docs);
+			response.status(200)
+			.json(docs);
+		});
+	}
+
+	/**
+	 * Return a series airing on a specitid date by its ID number
+	 * @description example URL - http://localhost:3000/api/v1/series/45/date/2017-05-04T16:30:00
+	 * @ param {String} request - the request string
+	 * @ param {String} response - the response string
+	 * @ member {Function} scheduleCollection - links to mongoDB collection
+	 * @ external {}
+	 */
+	getSeriesByDate(request, response) {
+		let scheduleCollection = mongoDB.collection('scheduleData');
+		let seriesID = parseInt(request.params.id);
+		let requestedDate = request.params.date;
+
+		scheduleCollection.find( { $and : [
+			{ 'series.series_id' : seriesID },
+			{ 'schedule.schedule_date' : { '$eq' : requestedDate } }
+			] } )
+		.limit( 10 )
+		.toArray(function(error, docs) {
+			if (error) {
+				console.log(error);
+			}
+			response.status(200)
+			.json(docs);
+		});
+	}
+
+	/**
+	 * Return series airings for a specitid date range by its ID number
+	 * @description example URL - http://localhost:3000/api/v1/series/45/startdate/2017-05-04T16:30:00Z/enddate/2017-05-28T16:30:00Z
+	 * @ param {String} request - the request string
+	 * @ param {String} response - the response string
+	 * @ member {Function} scheduleCollection - links to mongoDB collection
+	 * @ external {}
+	 */
+	getSeriesByDateRange(request, response) {
+		let scheduleCollection = mongoDB.collection('scheduleData');
+		let seriesID = parseInt(request.params.id);
+		let requestedStartDate = request.params.startDate;
+		let requestedEndDate = request.params.endDate;
+
+		scheduleCollection.find( { $and : [
+			{ 'series.series_id' : seriesID },
+			{ 'schedule.schedule_date' : {
+				'$gte' : requestedStartDate,
+				'$lte' : requestedEndDate }
+			}
+       ] } )
+		.limit( 10 )
+		.toArray(function(error, docs) {
+			if (error) {
+				console.log(error);
+			}
+			response.status(200)
+			.json(docs);
+		});
+	}
+
+	// http://localhost:3000/api/v1/channels/genre/CH
+
+	getByGenreCode(request, response) {
+		let scheduleCollection = mongoDB.collection('scheduleData');
+		let genreCode = request.params.genreCode;
+
+		scheduleCollection.find( {
+			'episode.epi_genrelist_nat.0.genrecd' : genreCode
+		} )
+		.limit( 10 )
+		.toArray(function(error, docs) {
+			if (error) {
+				console.log(error);
+			}
+			response.status(200)
+			.json(docs);
 		});
 	}
 
@@ -34,6 +123,9 @@ export default class ChannelRouter {
 	 * Attach route handlers to their endopoints
 	 */
 	init() {
-		this.router.get('/', this.getAll);
-	}	
+		this.router.get('/:id', this.getSeriesById);
+		this.router.get('/:id/date/:date', this.getSeriesByDate);
+		this.router.get('/:id/startdate/:startDate/enddate/:endDate', this.getSeriesByDateRange);
+		this.router.get('/:id/genre/:genreCode', this.getByGenreCode);
+	}
 }
