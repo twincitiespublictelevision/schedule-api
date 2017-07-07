83f61a00dd7d657d7a952ee648f83827af5b0059
Travis Ingram <ingramtravis@gmail.com>
2017-07-07 16:48:46 -0500

    scheduleAPIv2 database.js
    Add JSDoc blocks to all database functions,
    Update the initialization and connection function names to
    better represent what they do

--- src/database.js
+++ src/database.js
@@ -1,21 +1,36 @@
 import mongoose from 'mongoose';
-import mongodb from 'mongodb';
+// import mongodb from 'mongodb';
 
 const mongoURI = 'mongodb://localhost:27017/scheduleData';
 const mongoDB = mongoose.connect(mongoURI).connection;
 
-function initialize() {
+/**
+ * Event listener for error when conneting to the database.
+ * @ member {Function} scheduleCollection - links to mongoDB collection
+ */
+function databaseConnectionError() {
 	mongoDB.on('error', function(error, db) {
 		console.log('mongoDB connection error.', error);
 	});
 }
 
-function connect() {
+/**
+ * Open connection to the database
+ * @ member {Function} mongoDB - link to collection
+ */
+function databaseConnection() {
 	mongoDB.once('open', function() {
 		console.log('mongoDB connection established.');
 	});
 }
 
+/**
+ * Inssert a single object into the database
+ * @ param {String} mongoDB - the connection to the database
+ * @ param {Object} data - the object being inserted into the database
+ * @ param (Function) callback - Do something with the results
+ * @ member {Function} scheduleCollection - links to mongoDB collection
+ */
 function singleInsert(mongoDB, data, callback) {
 	let scheduleCollection = mongoDB.collection('scheduleData');
 
@@ -26,6 +41,13 @@ function singleInsert(mongoDB, data, callback) {
 	});
 }
 
+/**
+ * Insert N number of objects into the database
+ * @ param {String} mongoDB - the connection to the database
+ * @ param {Object} data - the objects being inserted into the database
+ * @ param (Function) callback - Do something with the results
+ * @ member {Function} scheduleCollection - links to mongoDB collection
+ */
 function multipleInsert(mongoDB, data, callback) {
 	let scheduleCollection = mongoDB.collection('scheduleData');
 
@@ -36,6 +58,13 @@ function multipleInsert(mongoDB, data, callback) {
 	});
 }
 
+/**
+ * Remove all of the objects in the current collection before inserting more
+ * @ param {String} mongoDB - the connection to the database
+ * @ param {Object} data - the objects being inserted into the database
+ * @ param (Function) callback - Do something with the results
+ * @ member {Function} scheduleCollection - links to mongoDB collection
+ */
 function removeAllInsert(mongoDB, data, callback) {
 	let scheduleCollection = mongoDB.collection('scheduleData');
 
@@ -51,6 +80,13 @@ function removeAllInsert(mongoDB, data, callback) {
 	});
 }
 
+/**
+ * Remove all of the objects in the current collection
+ * @ param {String} mongoDB - the connection to the database
+ * @ param {Object} data - the emplty object being inserted into the database
+ * @ param (Function) callback - Do something with the results
+ * @ member {Function} scheduleCollection - links to mongoDB collection
+ */
 function removeAll(mongoDB, data, callback) {
 	let scheduleCollection = mongoDB.collection('scheudleData');
 
@@ -61,6 +97,13 @@ function removeAll(mongoDB, data, callback) {
 	});
 }
 
+/**
+ * Find all of the objects in the current collection
+ * @ param {String} mongoDB - the connection to the database
+ * @ param {Object} data - the objects being inserted into the database
+ * @ param (Function) callback - Do something with the results
+ * @ member {Function} scheduleCollection - links to mongoDB collection
+ */
 function findAll(fn) {
 	let scheduleCollection = mongoDB.collection('scheduleData');
 
@@ -75,8 +118,8 @@ function findAll(fn) {
 export {
 	mongoURI,
 	mongoDB,
-	initialize,
-	connect,
+	databaseConnectionError,
+	databaseConnection,
 	singleInsert,
 	multipleInsert,
 	removeAllInsert,
