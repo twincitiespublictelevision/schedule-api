896e2d6c0d40512f74c7b74eafd6329ebc2ac96b
Travis Ingram <ingramtravis@gmail.com>
2017-07-14 15:48:57 -0500

    scheduleAPIv2 parseFileSaveData.js
    Remove removeMultiipleInsert function and replace with
    removeOldInsertNew, which is located in database.js,
    and unused imports to clean up file

--- src/parseFileSaveData.js
+++ src/parseFileSaveData.js
@@ -3,31 +3,14 @@ import * as path from 'path';
 import { jsonScheduleData } from './helpers/convertXMLToJSON';
 
 import {
-	mongoURI,
 	mongoDB,
-	multipleInsert,
-	removeAllInsert,
-	removeAll,
-	convertDates
+	removeOldInsertNew,
 	} from './database';
 
 import {
 	removeSingleFile,
 	extractScheduleData
 } from './helpers/importScheduleData';
-
-function removeMultipleInsert(mongoDB, fullScheduleData, file, callback) {
-	let scheduleCollection = mongoDB.collection('scheduleData');
-
-	scheduleCollection.remove({'schedule.schedule_channel': fullScheduleData[0].schedule.schedule_channel}, function(){
-		scheduleCollection.insertMany(fullScheduleData, function(error, result) {
-			if(error) {
-				console.log(error);
-			}
-				convertDates(result);
-			});
-	});
-};
 
 function handleFile(file) {
 	let filename = path.basename(file);
@@ -38,7 +21,7 @@ function handleFile(file) {
 		} else {
 			let scheduleData = extractScheduleData(parseData);
 			console.log(`Starting DB write of ${filename}.`);
-			removeMultipleInsert(mongoDB, scheduleData, filename);
+			removeOldInsertNew(mongoDB, scheduleData);
 		}
 		removeSingleFile(file);
 	});
