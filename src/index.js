ebbfd937bb87ad6c6aa253694512da2827f8099e
Travis Ingram <ingramtravis@gmail.com>
2017-07-19 13:36:03 -0500

    TPTOR-2008 index.js
    Add initial supprot for TSL/SSL for both the server
    and database connection.
    Implemented preliminary solution for a global redirect from
    HTTP to HTTPS, will update after review

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 'use strict'
 
+import * as fs from 'fs';
 import * as http from 'http';
+import * as https from 'https';
 import Api from './api';
 import {
 	databaseConnectionError,
@@ -11,27 +13,39 @@ import {
 
 const app = new Api();
 const DEFAULT_PORT = 3000;
-const port = normalizePort(process.env.SERVER_PORT);
-const server = http.createServer(app.express);
+const httpPort = normalizePort(process.env.HTTP_SERVER_PORT);
+// const httpServer = http.createServer(app.express);
+
+const httpServer = http.createServer(function (request, response) {
+    response.writeHead(301, { 'Location': 'https://0.0.0.0:3443' + request.url });
+    response.end();
+})
+
+// HTTPS Connection Constants
+const httpsPort = httpsNormalizePort(process.env.HTTPS_SERVER_PORT);
+const httpsPrivateKey  = fs.readFileSync(process.env.HTTPS_PRIVATEKEY, 'utf8');
+const httpsCertificate = fs.readFileSync(process.env.HTTPS_CERTIFICATE, 'utf8');
+const httpsCredentials = {key: httpsPrivateKey, cert: httpsCertificate};
+const httpsServer = https.createServer(httpsCredentials, app.express);
 
 // DB Connection
 
 databaseConnectionError();
 databaseConnection();
 
-// Server Connection
+// HTTP Server Connection
 
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+httpServer.listen(httpPort);
+httpServer.on('error', onError);
+httpServer.on('listening', onListening);
 
 function normalizePort(val) {
-	let port = (typeof val === 'string') ? parseInt(val, 10) : val;
+	let httpPort = (typeof val === 'string') ? parseInt(val, 10) : val;
 
-	if (port && isNaN(port)) {
-		return port;
-	} else if (port >= 0) {
-			return port;
+	if (httpPort && isNaN(httpPort)) {
+		return httpPort;
+	} else if (httpPort >= 0) {
+			return httpPort;
 	} else {
 			return DEFAULT_PORT;
 	}
@@ -42,7 +56,7 @@ function onError(error) {
 		throw error;
 	}
 
-	let bind = (typeof port === 'string') ? `Pipe ${port}` : `Port ${port.toString()}`;
+	let bind = (typeof httpPort === 'string') ? `Pipe ${httpPort}` : `Port ${httpPort.toString()}`;
 
 	switch (error.code) {
 		case 'EACCESS':
@@ -59,7 +73,52 @@ function onError(error) {
 }
 
 function onListening() {
-	let addr = server.address();
+	let addr = httpServer.address();
+	let bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
+	console.log(`Listening on ${bind}`);
+}
+
+// HTTPS Server Connection
+
+httpsServer.listen(httpsPort);
+httpsServer.on('error', httpsOnError);
+httpsServer.on('listening', httpsOnListening);
+
+function httpsNormalizePort(val) {
+	let httpsPort = (typeof val === 'string') ? parseInt(val, 10) : val;
+
+	if (httpsPort && isNaN(httpsPort)) {
+		return httpsPort;
+	} else if (httpsPort >= 0) {
+			return httpsPort;
+	} else {
+			return DEFAULT_PORT;
+	}
+}
+
+function httpsOnError(error) {
+	if (error.syscall !== 'listen') {
+		throw error;
+	}
+
+	let bind = (typeof httpsPort === 'string') ? `Pipe ${httpsPort}` : `Port ${httpsPort.toString()}`;
+
+	switch (error.code) {
+		case 'EACCESS':
+			console.error(`${bind} is already in use.`);
+			process.exit(1);
+			break
+		case 'EADDRINUSE':
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+		default:
+			throw error;
+	}
+}
+
+function httpsOnListening() {
+	let addr = httpsServer.address();
 	let bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
 	console.log(`Listening on ${bind}`);
 }
